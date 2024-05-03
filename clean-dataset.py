2e994010605c311cc95024b615630a56ec476d8a
Roman Urban-Orban <roman.urban.orban@gmail.com>
2024-05-04 02:05:25 +0300

    cleanup comments

--- clean-dataset.py
+++ clean-dataset.py
@@ -1,26 +1,22 @@
 import json
 import os
 
-# Load the JSON data from the original data.json file
 with open('data/data.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
-# This list will store the entries for which the image file exists or imageURL is not None
 filtered_data = []
 
 for i, entry in enumerate(data):
-    # Check if 'imageURL' exists and is not None
     if entry.get('imageURL'):
-        # Extract the file name from the imageURL
-        image_url = entry['imageURL']
-        file_name = image_url.split('/')[-1]  # Adjust this line if needed
 
-        # Construct the file path assuming the images are stored in the data/ directory
+        image_url = entry['imageURL']
+        file_name = image_url.split('/')[-1] 
+
         file_path = os.path.join('data', file_name)
 
-        # Check if the file exists on the disk
+        # check if the file exists on the disk
         if os.path.exists(file_path):
-            # If the file exists, add the entry to the filtered_data list
+            # if the file exists, add the entry to the filtered_data list
             filtered_data.append(entry)
             print(f"File exists and retained for entry {i+1}: {file_name}")
         else:
@@ -28,7 +24,7 @@ for i, entry in enumerate(data):
     else:
         print(f"No valid imageURL for entry {i+1}, entry will be removed.")
 
-# Write the filtered data back to a new JSON file
+# write the filtered data back to a new JSON file
 with open('data/data_clean.json', 'w', encoding='utf-8') as file:
     json.dump(filtered_data, file, indent=4, ensure_ascii=False)
 
